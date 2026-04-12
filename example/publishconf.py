7f3f8ef99308a3a9b1aec7e463b321ce40694420
Fidel Ramos <contact.gyldd@8shield.net>
2026-04-12 18:06:39 +0100

    Add STORK_INPUT_OPTIONS to example site

--- example/publishconf.py
+++ example/publishconf.py
@@ -16,6 +16,12 @@ USE_LESS = False
 
 PLUGINS = ['pelican.plugins.search']
 
+STORK_INPUT_OPTIONS = {
+    "break_on_file_error": True,
+    "stemming": "english",
+    "url_prefix": SITEURL,
+}
+
 SHYNET_URL = (
     "https://shynet.fidelramos.net/ingress/9574b461-0b4e-4e54-adfc-1d49735686f1"
 )
